1a85ab1f89e9acd8341bbcd1fd5f97fae7fbb54c
agmonster08 <26757790+agmonster08@users.noreply.github.com>
2025-02-25 21:46:30 -0500

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Arismendy Gill on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
